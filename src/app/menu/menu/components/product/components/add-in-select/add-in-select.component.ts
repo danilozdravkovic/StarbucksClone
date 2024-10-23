afeba66012ca2c7217863ad5a5c28bdd39aa5b37
danilozdravkovic <danilo.zdravkovic01.biz@gmail.com>
2024-10-23 23:03:28 +0200

    Product add to cart in progress

--- src/app/menu/menu/components/product/components/add-in-select/add-in-select.component.ts
+++ src/app/menu/menu/components/product/components/add-in-select/add-in-select.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-add-in-select',
@@ -6,32 +7,95 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./add-in-select.component.css']
 })
 export class AddInSelectComponent {
+
+  
+  constructor(private fb: FormBuilder) {
+  }
   @Input() addIns: any ;
   @Input() parentName: string = '';
   subchildId : number=3;
 
-  hasSubchildPump(addIn: any): boolean {
+  addInForm: FormGroup;
+
+  ngOnInit(): void {
+    this.addInForm = this.fb.group({});
+
+    this.addIns?.forEach((addIn:any) => {
+      if(addIn.children){
+        this.addInForm.addControl(String(addIn.id), new FormControl(this.getSelectedSubchildId(addIn.children)));
+      }
+      else{
+        this.addInForm.addControl(String(addIn.id), new FormControl(addIn.pump));
+      }
+      
+    });
+
+    this.addInForm.valueChanges.subscribe(() => {
+      console.log(this.addIns);
+      const addInData = this.addIns.map((addIn: any) => {
+        console.log(addIn)
+        if (addIn.children && addIn.children.length > 0) {
+          const subchildWithPump = addIn.children.find((child: any) => !this.subchildHasNoPump(child));
+          console.log(subchildWithPump);
+          if (subchildWithPump) {
+            console.log(addIn);
+            return {
+              id: addIn.children[0].id,
+              pump: addIn.children[0].pump
+            };
+          }
+        }
+  
+        return {
+          id: this.addInForm.get(String(addIn.id))?.value,
+          pump: addIn.pump
+        };
+      });
+      
+      console.log(addInData);
+    });
+    
+  }
+
+  subchildHasNoPump(addIn: any): boolean {
     return addIn.pump == null;
   }
 
   hasChildrenForSelect(array: any[]): boolean {
-    return array.some(subchild => !subchild.children && this.hasSubchildPump(subchild));
+    return array.some(subchild => !subchild.children && this.subchildHasNoPump(subchild));
   }
 
   incrementPump(addIn: any): void {
     if(addIn.pump<12){
       addIn.pump++;
+      this.addInForm.get(String(addIn.id))?.setValue(addIn.pump);
     }
   }
 
   decrementPump(addIn: any): void {
     if (addIn.pump > 0) {
       addIn.pump--;
+      this.addInForm.get(String(addIn.id))?.setValue(addIn.pump);
     }
   }
 
   getSelectedSubchildId(children: any[]): string {
     const selectedSubchild = children.find(subchild => subchild.selected);
     return selectedSubchild ? selectedSubchild.id : null;
+  }
+
+  buildAddInControls(): FormGroup[] {
+    return this.addIns.map((addIn:any) => this.fb.group({
+      id: new FormControl(addIn.id),
+      pump: new FormControl(addIn.pump ?? 0) 
+    }));
+  }
+
+  submitForm() {
+    const selectedAddIns = this.addIns.map((addIn:any) => ({
+        id: addIn.id,
+        pump: addIn.pump,
+    }));
+    console.log(selectedAddIns);
   }
   }
--- src/app/menu/menu/components/product/product.component.ts
+++ src/app/menu/menu/components/product/product.component.ts
@@ -9,43 +9,51 @@ import { IProduct } from 'src/app/menu/interfaces/i-product';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent implements OnInit{
 
   constructor(private productsService:ProductsService,
               private route: ActivatedRoute,
               private router:Router,
               private cdr: ChangeDetectorRef) {}
   
-  productToDisplay:any;
+  productToDisplay: any = null;
   calories: number = 0;
   caloriesToDisplay : number=0;
+  isViewInitialized: boolean = false;
+  selectedSizeId:number;
 
   ngOnInit() : void{
     const productId : number= +this.route.snapshot.paramMap.get('id')!;
-    this.productsService.getOne(productId).subscribe(product =>{
-      if(product){
-        this.productToDisplay=product;
-        console.log(this.productToDisplay.sizes.length);
-        this.caloriesToDisplay=product.calories;
-        this.calories=product.calories;
-      }
-      else{
-        this.router.navigate(['**'])
-      }
+    this.productsService.getOne(productId).subscribe({
+      next:(product)=>{
+        if(product){
+          this.productToDisplay=product;
+          this.caloriesToDisplay=product.calories;
+          this.calories=product.calories;
+        }
+        else{
+          this.router.navigate(['**'])
+        }
+      },
+     error:(err)=>{
+        console.log(err);
+     }
   });
     }
 
-    ngAfterViewInit(){
-      const selectedSize = document.getElementsByClassName("cupSize")[0];
-      this.isActive(selectedSize,this.productToDisplay.sizes[0].additionalCalories,this.productToDisplay.sizes);
-      this.cdr.detectChanges();
-     }
-
-     
+  ngAfterViewChecked():void{
+    if (this.productToDisplay && !this.isViewInitialized) {
+          const selectedSize = document.getElementsByClassName("cupSize")[0];
+          this.isActive(selectedSize, this.productToDisplay.sizes[0].additionalCalories, this.productToDisplay.sizes);
+          this.isViewInitialized = true; 
+          this.cdr.detectChanges(); 
+      }
+    }
 
 //function first removes all active links from cupSize divs and then checks on witch div is clicked so it can give it active class
   isActive(div:HTMLDivElement | Element,additionalCalories:number,allSizes: any[]):void{
     let divs =document.getElementsByClassName("cupSize");
+    this.selectedSizeId=Number(div.getAttribute('id'));
     let divsArray = Array.from(divs);
     let allSizesNames: string[] = [];
     allSizes.forEach(element => {
